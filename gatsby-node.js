02bdeeb3f52789dca8d91c3ce37b525480e87024
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2025-03-22 07:43:52 -0400

    require path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
+const path = require("path")
 
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
 
